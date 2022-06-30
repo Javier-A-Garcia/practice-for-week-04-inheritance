6685d7b84236c6e4c931a8e729c5f36868a5c52a
Javier Garcia <javiergarcia7991@gmail.com>
2022-06-30 06:12:01 -0700

    Completed practice

--- classes/teacher.js
+++ classes/teacher.js
@@ -2,6 +2,20 @@ const Person = require('./person');
 
 // Your code here
 
+class Teacher extends Person {
+  constructor(name, age, subject, yearsOfExperience) {
+    super(name, age);
+    this.subject = subject;
+    this.yearsOfExperience = yearsOfExperience;
+  }
+
+  static combinedYearsOfExperience(teachers) {
+    return teachers.reduce((previous, current) => {
+      return previous.yearsOfExperience + current.yearsOfExperience
+    });
+  }
+}
+
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
